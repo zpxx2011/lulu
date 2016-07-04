c8c0d2ecd0fcb72dea4a7aeb462f8a21c77cf90c
zhaoping <zpxx2014@163.com>
2016-07-04 10:47:18 +0800

    Initial commit.

---
+++ main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  YLLoopScrollView
+//
+//  Created by Youngly on 16/3/18.
+//  Copyright © 2016年 YL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "YLAppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([YLAppDelegate class]));
+    }
+}
